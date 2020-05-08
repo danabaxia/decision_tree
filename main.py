ba74ac1d42f7caf6d9b66c61322aad542069221f
Jingbo Han <jingbo_han@hq.autometer.com>
2020-05-08 17:06:40 -0500

    second

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("Hello")
+import math 
+
+def cal_I(m,n):
+
+    return -m/(m+n)*math.log2(m/(m+n))-(n/(m+n)*math.log2(n/(m+n)))
+
+
+
+training_data_num = 0
+training_data_single = {}
+training_data = {}
+#read file 
+with open("data.txt") as fp:
+    training_data_num = int(fp.readline())
+    for line in fp.readlines(): 
+        for element in line.split():
+            print(element)
