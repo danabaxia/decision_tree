ed4b495af37577aebbbc5913b77b1b1489bcf68b
danabaxia <danabaxia@gmail.com>
2020-05-10 13:48:31 -0500

    modify bestFeature function, input only dataset

--- main.py
+++ main.py
@@ -26,30 +26,99 @@ def get_attribute_data(data, num, order):
         line = fp.readline().split()
         attribute.append(line[order + 1].split(':')[1])
     return attribute
+
+def cal_I(m,n,l):
+    t = m + n + l
+    return -(m/t)*math.log2(m/t) - (n/t)*math.log2(n/t) - (l/t)*math.log2(l/t)
     
+def cal_entropy(label):
+    table = {}
+    label_value = []
+    entropy = 0
+    total = len(label)
+    for item in label:
+        if item not in label_value:
+            label_value.append(item)
 
+    for item in label_value:
+        table[item] = label.count(item)
+    #print(table)    
+    for key in table:
+        entropy += -(table[key]/total)*math.log2(table[key]/total)
+        #print(key, table[key], total)
+    return entropy
 
-def cal_I(m,n):
-    return -m/(m+n)*math.log2(m/(m+n))-(n/(m+n)*math.log2(n/(m+n)))
+def cal_best_feature(data):
+    training_data_num = get_train_data_num(data) # training data sample number
+    label = get_labels(data, training_data_num) # label list 
+    attribute_num = get_attribute_num(data, training_data_num) # attribute number 
+    attribute = []
+    for i in range(attribute_num):
+        attribute.append(get_attribute_data(data, training_data_num, i)) # nested list of attributes and its values in training data
+
+    bestGain = 0
+    bestFeature = -1
+    baseEntropy = cal_entropy(label)
+    for i in range(len(attribute)):
+        values = [] # all the values in one feature 
+        newEntropy = 0
+        #print(feature)
+        for value in attribute[i]:
+            if value not in values:
+                values.append(value)
+        #print(values)
+        
+        feature = attribute[i]
+        for value in values: #count labels for different values
+            table = []
+            for i in range(training_data_num):
+                if feature[i] == value:
+                    table.append(label[i])
+            prob = len(table)/len(label)  
+            newEntropy += prob * cal_entropy(table) # cal info for one feature 
+        Gain = baseEntropy - newEntropy # cal info 
+        #print(Gain)
+        if (Gain > bestGain):
+            bestGain = Gain
+            bestFeature = i
+    return bestFeature
+"""
+def createTree(label, attribute, training_data_num):
+    if label.count(label[0]) == len(label):
+        return label[0]  # only one label 
+
+    bestFeature = cal_best_feature(label, attribute, training_data_num)
+    bestFeat = tuple(attribute[bestFeature])
+    print(bestFeat)
+    myTree = {bestFeat:{}} 
+    print(myTree)
+    del attribute[bestFeature]
+    valueList = set(attribute[bestFeature])
+    print(valueList)
+    for value in valueList: 
+        myTree[bestFeat][value] = createTree()
+
+"""
+
 
 
 data = "data.txt"
-training_data_num = 0
-attribute_num = 0
-label = []
+#training_data_num = 0
+#attribute_num = 0
+#label = []
+#entropy = 0
+bestFeature = cal_best_feature(data)
 
 
-training_data_num = get_train_data_num(data)
-label = get_labels(data, training_data_num)
-attribute_num = get_attribute_num(data, training_data_num)
-
-attribute = []
-for i in range(attribute_num):
-    attribute.append(get_attribute_data(data, training_data_num, i))
-
-print(training_data_num)
-print(label)
-print(attribute_num)
-for item in attribute:
-    print(item)
+"""
+bestFeature = cal_best_feature(data)
+bestFeat = tuple(attribute[bestFeature])
+print(bestFeat)
+myTree = {bestFeat:{}} 
+print(myTree)
+del attribute[bestFeature]
+valueList = set(attribute[bestFeature])
+print(valueList)
+for value in valueList:
+"""
 
