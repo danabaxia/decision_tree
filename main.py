cc1f970baa51a0a5c3b60c3d13a63729051398b2
danabaxia <danabaxia@gmail.com>
2020-05-09 22:11:19 -0500

    split data

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
 import math 
 
-def cal_I(m,n):
+def get_train_data_num(data):
+    fp = open(data)
+    return int(fp.readline())
 
+def get_labels(data, num):
+    label = []
+    fp = open(data)
+    fp.readline()
+    for x in range(num):
+        line = fp.readline().split()
+        label.append(line[0])
+    return label
+
+def get_attribute_num(data, num):
+    fp = open(data)
+    fp.readline()
+    return len(fp.readline().split()) - 1
+
+def get_attribute_data(data, num, order):
+    attribute = []
+    fp = open(data)
+    fp.readline()
+    for x in range(num):
+        line = fp.readline().split()
+        attribute.append(line[order + 1].split(':')[1])
+    return attribute
+    
+
+
+def cal_I(m,n):
     return -m/(m+n)*math.log2(m/(m+n))-(n/(m+n)*math.log2(n/(m+n)))
 
 
-
+data = "data.txt"
 training_data_num = 0
-training_data_single = {}
-training_data = {}
-#read file 
-with open("data.txt") as fp:
-    training_data_num = int(fp.readline())
-    for line in fp.readlines(): 
-        for element in line.split():
-            print(element)
+attribute_num = 0
+label = []
+
+
+training_data_num = get_train_data_num(data)
+label = get_labels(data, training_data_num)
+attribute_num = get_attribute_num(data, training_data_num)
+
+attribute = []
+for i in range(attribute_num):
+    attribute.append(get_attribute_data(data, training_data_num, i))
+
+print(training_data_num)
+print(label)
+print(attribute_num)
+for item in attribute:
+    print(item)
+
